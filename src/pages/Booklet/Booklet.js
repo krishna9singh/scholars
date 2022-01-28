0a7fa71172d6418b851c96e9fd5de0d6aa542324
Shreyansh Singh <traceit241@gmail.com>
2022-01-28 02:31:34 -0800

    Update Booklet.js

--- src/pages/Booklet/Booklet.js
+++ src/pages/Booklet/Booklet.js
@@ -39,10 +39,196 @@ function Booklet() {
     <AC>Ans- Peripherals</AC>
     <QC>Q5- Full form of HTML?</QC>
     <AC>Ans- Hyper text markup language</AC>
-    <QC>Q6- which tag defines a section which is quoted form anthor source ?</QC>
-    <AC>Ans- blockquoted</AC>
+    <QC>Q6- Which tag defines a section which is quoted form another source ?</QC>
+    <AC>Ans- Blockquote tag</AC>
     <QC>Q7- A person wants to place links from navigation which tag he/she use?</QC>
-    <AC>Ans- i</AC>
+    <AC>Ans- nav tag</AC>
+    <QC>Q8-Which tag defines a comment?</QC>
+    <AC>Ans- Comment tag</AC>
+    <QC>Q9- Which tag defines the document type?</QC>
+    <AC>Ans- Doctype</AC>
+    <QC>Q10- Which tag defines hyperlinks?</QC>
+    <AC>Ans- Anchor tag</AC>
+    <QC>Q11- Which tag defines abbreviations or acronym?</QC>
+    <AC>Ans-abbr tag</AC>
+    <QC>Q12- Whijch tag dfines contact information for the author/owner of a document?</QC>
+    <AC>Ans- address tag</AC>
+    <QC>Q13- Which tag defines an area inside an image map?</QC>
+    <AC>Ans- area tag</AC>
+    <QC>Q14- Which tag defines an article?</QC>
+    <AC>Ans- article tag</AC>
+    <QC>Q15- Which tag defines content aside from the page content?</QC>
+    <AC>Ans- aside tag</AC>
+    <QC>Q16- Which tag defines embedded sound content?</QC>
+    <AC>Ans- audio tag</AC>
+    <QC>Q17- Which tag defines bold text?</QC>
+    <AC>Ans- b tag</AC>
+    <QC>Q18- Which tag defines Specifies the base URL/target for all relative URLs in a document?</QC>
+    <AC>Ans- base tag</AC>
+    <QC>Q19- Which tag isolates a part of text that might be formatted in a different direction from other text outside it?</QC>
+    <AC>Ans- bdi tag</AC>
+    <QC>Q20- Which tag overrides the current text direction?</QC>
+    <AC>Ans- bdo tag</AC>
+    <QC>Q21- which tag defines a section that is quoted from another source?</QC>
+    <AC>Ans- blockquote tag</AC>
+    <QC>Q22- Which tag defines the document's body?</QC>
+    <AC>Ans- body tag</AC>
+    <QC>Q23- Which tag defines a single line break?</QC>
+    <AC>Ans- BR tag</AC>
+    <QC>Q24- Which tag defines a clickable button?</QC>
+    <AC>Ans- button tag</AC>
+    <QC>Q25- tag used to draw graphics, on the fly?</QC>
+    <AC>Ans- Canvas tag</AC>
+    <QC>Q26- Which tag defines a table caption?</QC>
+    <AC>Ans- Caption tag</AC>
+    <QC>Q27- Which tag defines centered text?</QC>
+    <AC>Ans- center tag</AC>
+    <QC>Q28- Which tag defines the title of a work?</QC>
+    <AC>Ans- cite tag</AC>
+    <QC>Q29- Which tag defines a piece of computer code?</QC>
+    <AC>Ans- code tag</AC>
+    <QC>Q30- Which tag specifies column properties for each column within a colgroup element? </QC>
+    <AC>Ans- col tag</AC>
+    <QC>Q31- Which tag specifies a group of one or more columns in a table for formatting?</QC>
+    <AC>Ans- colgroup tag</AC>
+    <QC>Q32- Which tag adds a machine-readable translation of a given content?</QC>
+    <AC>Ans- data tag</AC>
+    <QC>Q33- Which tag specifies a list of pre-defined options for input controls?</QC>
+    <AC>Ans- datalist tag</AC>
+    <QC>Q34 -Which tag defines a description/value of a term in a description list?</QC>
+    <AC>Ans- dd tag</AC>
+    <QC>Q35- Which tag defines text that has been deleted from a document?</QC>
+    <AC>Ans- del tag</AC>
+    <QC>Q36- Which tag defines additional details that the user can view or hide?</QC>
+    <AC>Ans- details tag</AC>
+    <QC>Q37- Which tag specifies a term that is going to be defined within the content?</QC>
+    <AC>Ans- dfn tag</AC>
+    <QC>Q38- Whch tag defines a dialog box or window?</QC>
+    <AC>Ans- dialog tag</AC>
+    <QC>Q39- Which tag defines a section in a document?</QC>
+    <AC>Ans- div tag</AC>
+    <QC>Q40- Which tag defines a description list?</QC>
+    <AC>Ans- dl tag</AC>
+    <QC>Q41- Which tag d?efines a term/name in a description list?</QC>
+    <AC>Ans- dt tag</AC>
+    <QC>Q42- Which tag defines emphasized text?</QC>
+    <AC>Ans- em tag</AC>
+    <QC>Q43- Which tag defines a container for an external application?</QC>
+    <AC>Ans- embed tag</AC>
+    <QC>Q44- Which tag groups related elements in a form?</QC>
+    <AC>Ans- fieldset tag</AC>
+    <QC>Q45- Which tag defines a caption for a figure element? </QC>
+    <AC>Ans- figcaption tag</AC>
+    <QC>Q46- Which  tag specifies self-contained content?</QC>
+    <AC>Ans- figure tag</AC>
+    <QC>Q47- Which tag defines a footer for a document or section? </QC>
+    <AC>Ans- footer tag</AC>
+    <QC>Q48- Which tag defines an HTML form for user input?</QC>
+    <AC>Ans- form tag</AC>
+    <QC>Q49- Which tag defines HTML headings?</QC>
+    <AC>Ans- h1 to h6 tags</AC>
+    <QC>Q50- Which tag contains metadata/information for the document?</QC>
+    <AC>Ans- head tag </AC>
+    <QC>Q51- Which tag defines a header for a document or section</QC>
+    <AC>Ans- header tag</AC>
+    <QC>Q52- Which tag defines a thematic change in the content?</QC>
+    <AC>Ans- hr tag</AC>
+    <QC>Q53- Which tag defines the root of an HTML document?</QC>
+    <AC>Ans- html tag</AC>
+    <QC>Q54- Which tag defines an inline frame?</QC>
+    <AC>Ans- iframe tag</AC>
+    <QC>Q55- Which tag defines an image?</QC>
+    <AC>Ans- img tag</AC>
+    <QC>Q56- Which tag defines an input control?</QC>
+    <AC>Ans- input tag</AC>
+    <QC>Q57- Which tag defines a text that has been inserted into a document?</QC>
+    <AC>Ans- ins tag</AC>
+    <QC>Q58- Which tag defines keyboard input?</QC>
+    <AC>Ans- kbd tag</AC>
+    <QC>Q59- Which tag defines a label for an input element?</QC>
+    <AC>Ans- label tag</AC>
+    <QC>Q60- Which tag defines a caption for a fieldset element?</QC>
+    <AC>Ans- legend tag</AC>
+    <QC>Q61- Which tag defines a list item?</QC>
+    <AC>Ans- li tag</AC>
+    <QC>Q62- Which tag defines the relationship between a document and an external resource (most used to link to style sheets)?</QC>
+    <AC>Ans- link tag</AC>
+    <QC>Q63- Which tag specifies the main content of a document?</QC>
+    <AC>Ans- main tag</AC>
+    <QC>Q64- Which tag defines an image map?</QC>
+    <AC>Ans- map tag</AC>
+    <QC>Q65- Which tag defines marked/highlighted text?</QC>
+    <AC>Ans- mark tag</AC>
+    <QC>Q66- Which tag define metadata about an HTML document?</QC>
+    <AC>Ans- meta tag</AC>
+    <QC>Q67- Which tag define a scalar measurement within a known range (a gauge)?</QC>
+    <AC>Ans- meter tag</AC>
+    <QC>Q68- Which tag define navigation links?</QC>
+    <AC>Ans- nav tag</AC>
+    <QC>Q69- Which tag define an alternate content for users that do not support client-side scripts? </QC>
+    <AC>Ans- noscript tag</AC>
+    <QC>Q70- Which tag define a container for an external application?</QC>
+    <AC>Ans- object tag</AC>
+    <QC>Q71- Which tag define  an ordered list?</QC>
+    <AC>Ans- OL tag</AC>
+    <QC>Q72- Which tag define a group of related options in a drop-down list?</QC>
+    <AC>Ans- optgroup tag</AC>
+    <QC>Q73- Which tag define  an option in a drop-down list?</QC>
+    <AC>Ans- option tag</AC>
+    <QC>Q74- Which tag define  the result of a calculation?</QC>
+    <AC>Ans- output tag </AC>
+    <QC>Q75- Which tag define a paragraph?</QC>
+    <AC>Ans- p tag</AC>
+    <QC>Q76- WHich tag define  a parameter for an object?</QC>
+    <AC>Ans- param tag</AC>
+    <QC>Q77- Which tag define a container for multiple image resources?</QC>
+    <AC>Ans- picture tag</AC>
+    <QC>Q78- Which tag define preformatted text?</QC>
+    <AC>Ans- pre tag</AC>
+    <QC>Q79- Which tag represents the progress of a task?</QC>
+    <AC>Ans- progress tag</AC>
+    <QC>Q80- Wich tag defines a short quotation?</QC>
+    <AC>Ans- q tag</AC>
+    <QC>Q81- Which tag defines what to show in browsers that do not support ruby annotations?</QC>
+    <AC>Ans- rp tag</AC>
+    <QC>Q82- Which tag defines an explanation/pronunciation of characters (for East Asian typography)? </QC>
+    <AC>Ans- rt tag</AC>
+    <QC>Q83- Which tag defines a ruby annotation (for East Asian typography)?</QC>
+    <AC>Ans- ruby tag</AC>
+    <QC>Q84- Which tag defines text that is no longer correct?</QC>
+    <AC>Ans- s tag</AC>
+    <QC>Q85- Which tag defines sample output from a computer program?</QC>
+    <AC>Ans- samp tag</AC>
+    <QC>Q86- Which tag defines a client-side script?</QC>
+    <AC>Ans- script tag</AC>
+    <QC>Q87- Which tag defines a section in a document?</QC>
+    <AC>Ans- section tag</AC>
+    <QC>Q88- Which tag defines a drop-down list?</QC>
+    <AC>Ans- select tag</AC>
+    <QC>Q89- Which tag defines smaller text?</QC>
+    <AC>Ans- small tag</AC>
+    <QC>Q90- Which tag defines multiple media resources for media elements (video and audio)?</QC>
+    <AC>Ans- source tag</AC>
+    <QC>Q91- Which tag defines efines a section in a document?</QC>
+    <AC>Ans- span tag</AC>
+    <QC>Q92- Which tag defines important text?</QC>
+    <AC>Ans- strong tag</AC>
+    <QC>Q93- Which tag defines style information for a document?</QC>
+    <AC>Ans- style tag</AC>
+    <QC>Q94- Which tag defines subscripted text?</QC>
+    <AC>Ans- sub tag</AC>
+    <QC>Q95- Which tag defines a visible heading for a details element?</QC>
+    <AC>Ans- summary tag</AC>
+    <QC>Q96- Which tag defines superscripted text?</QC>
+    <AC>Ans- sup tag</AC>
+    <QC>Q97- Which tag defines a container for SVG graphics?</QC>
+    <AC>Ans- SVG tag</AC>
+    <QC>Q98- Which tag defines a table?</QC>
+    <AC>Ans- table tag</AC>
+    <QC>Q99- Which tag groups the body content in a table?</QC>
+    <AC>Ans- tbody</AC>
+    <QC>Q100- Which tag defines a cell in a table?</QC>
+    <AC>Ans- td tag</AC>
     
     </>
   );
