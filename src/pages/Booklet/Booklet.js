673d6d870801eae23e0da9f78ab20cd8f5f86b30
Shreyansh Singh <traceit241@gmail.com>
2022-02-01 21:20:20 -0800

    Update Booklet.js

--- src/pages/Booklet/Booklet.js
+++ src/pages/Booklet/Booklet.js
@@ -229,7 +229,110 @@ function Booklet() {
     <AC>Ans- tbody</AC>
     <QC>Q100- Which tag defines a cell in a table?</QC>
     <AC>Ans- td tag</AC>
+    <QC>Q101- Which programs reduce the size of the files so that they occupy lesser space on the disk?</QC>
+    <AC>Ans-  File Compression </AC>
+    <QC>Q102- Popular network operating systems include all of the following EXCEPT</QC>
+    <AC>Ans- Microsoft DOS</AC>
+    <QC>Q103- Type of software that can be described as “end-user” software</QC>
+    <AC>Ans- application software</AC>
+    <QC>Q104-  ________provides the user interface, controls the computers resources and runs programs.</QC>
+    <AC>Ans- Operating system</AC>
+    <QC>Q105-  ______________utility identifies non essential files on the hard disk and erases them only when user allows their erasure.</QC>
+    <AC>Ans- Disk Cleanup</AC>
+    <QC>Q106- Which of these operating system does not have a graphical user interface ?</QC>
+    <AC>Ans- Ms Dos</AC>
+    <QC>Q107- GUI stands for</QC>
+    <AC>Ans- Graphical User Interface</AC>
+    <QC>Q108-  _______ are also known as Service programs.</QC>
+    <AC>Ans- Utilities</AC>
+    <QC>Q109-  ______is a 16-bit code designed to support international languages like Chinese and Japanese.</QC>
+    <AC>Ans- Unicode</AC>
+    <QC>Q110- “Microprocessor” and “Memory” are the important component of ___.</QC>
+    <AC>Ans- System unit</AC>
+    <QC>Q111- From the given list, which is not a type of Memory Chip</QC>
+    <AC>Ans- ASCII</AC>
+    <QC>Q112- The smallest unit in a digital system is a</QC>
+    <AC>Ans- bit</AC>
+    <QC>Q113- ALU performs two types of operations,_____</QC>
+    <AC>Ans- Arithmetic and  Logical</AC>
+    <QC>Q114- ROM stands for,______.</QC>
+    <AC>Ans- Read only memory</AC>
+    <QC>Q115- Which among the following is smallest in size ?</QC>
+    <AC>Ans- PDA System Unit</AC>
+    <QC>Q116- A 32 bit word computer can access____ bytes at a time.</QC>
+    <AC>Ans- 4</AC>
+    <QC>Q117- Which of these is not an output device?</QC>
+    <AC>Ans- Joystick</AC>
+    <QC>Q118- Ink-jet, laser and thermal are common types of_____.</QC>
+    <AC>Ans- Printers</AC>
+    <QC>Q119- Which is the most  Common type of Audio-Output Device?</QC>
+    <AC>Ans- Speaker</AC>
+    <QC>Q120- The most important characteristic of a monitor is ?</QC>
+    <AC>Ans- Clarity</AC>
+    <QC>Q121- The keyboard keys that are labeled F1, F2 and so on are called</QC>
+    <AC>Ans- Function keys</AC>
+    <QC>Q122- Which of the the following device is not from pointing type of device?</QC>
+    <AC>Ans- Keyboard</AC>
+    <QC>Q123- Which of the following device is used to play fast computer games?</QC>
+    <AC>Ans- Joystick</AC>
+    <QC>Q124-  ________translates and executes program at run time line by line</QC>
+    <AC>Ans- Interpreter</AC>
+    <QC>Q125- Unwanted repetitious messages, such as unsolicited bulk e-mail is known as</QC>
+    <AC>Ans- Spam</AC>
+    <QC>Q126- Which among the following is responsible for allocating IP addresses and managing the domain name system?</QC>
+    <AC>Ans- Domain name warehousing</AC>
+    <QC>Q127- A generic name for Intel processors released after the original 8086 processor is ______ ?</QC>
+    <AC>Ans- Pentium</AC>
+    <QC>Q128- A network that is connected to the Internet, but uses encryption to scramble all the data sent through the Internet is called ?</QC>
+    <AC>Ans- My network</AC>
+    <QC>Q129- The software tool that enable a user to interact with computer for a specific purpose are known as?</QC>
+    <AC>Ans- Application</AC>
+    <QC>Q130- The performance of a hard drive or other storage device , meaning how long it takes to locate a file is called ?</QC>
+    <AC>Ans- Quick Time</AC>
+    <QC>Q131- The word bandwidth is also used to mean ________?</QC>
+    <AC>Ans- amount of data transferred</AC>
+    <QC>Q132- In order to save word document as a web page you need to?</QC>
+    <AC>Ans- use your web browser as an editor and save as URL </AC>
+    <QC>Q133- The ______ of system includes the programs or instructions.</QC>
+    <AC>Ans- icon </AC>
+    <QC>Q134- The advantage of DRAM is ?</QC>
+    <AC>Ans- It is cheaper than SRAM </AC>
+    <QC>Q135- A word in a web page that, when clicked, opens another document is called ___</QC>
+    <AC>Ans- hyperlink </AC>
+    <QC>Q136- One Terabyte (1 TB) is equal to?</QC>
+    <AC>Ans- 1204GB </AC>
+    <QC>Q137- Which operating system is developed and used by Apple Inc?</QC>
+    <AC>Ans- iOS </AC>
+    <QC>Q138- Which of the following places the common data elements in order from smallest to largest?</QC>
+    <AC>Ans- Character, Field, Record, File, Database </AC>
+    <QC>Q139- Which of these networks is usually the fastest?</QC>
+    <AC>Ans- LAN </AC>
+    <AC>Ans- Which of the following is a general-purpose network? </AC>
+    <AC>Ans- Local Area Network (LAN) </AC>
+    <QC>Q140- What is the IEEE standard for Gigabit Ethernet?</QC>
+    <AC>Ans- IEEE 802.3ab </AC>
+    <QC>Q141- Which of the following options acts as a barrier between local Area Network (LAN) and the Internet?</QC>
+    <AC>Ans- Firewall  </AC>
+    <QC>Q142- Which of the following networks uses telephone lines?</QC>
+    <AC>Ans- WAN </AC>
+    <QC>Q143- Which of the following types of Networks is larger than LAN but smaller than WAN?</QC>
+    <AC>Ans- MAN </AC>
+    <QC>Q144- For textual Chatting on internet, most common chat protocol is____.</QC>
+    <AC>Ans- IRC </AC>
+    <QC>Q145- In Infrastructure WLAN, two devices communicate-</QC>
+    <AC>Ans- Through an access Point </AC>
+    <QC>Q146- If computer in a LAN are connected along a single cable than the name given to such a topology is</QC>
+    <AC>Ans- Bus topology </AC>
+    <QC>Q147- The topology in which every node is connected only to its immediate neighbours is called:</QC>
+    <AC>Ans- Ring </AC>
+    <QC>Q148- Which of the following networks topology has more complex strcuture?</QC>
+    <AC>Ans- Hybird </AC>
+    <QC>Q149- What is full form of IOS ?</QC>
+    <AC>Ans- iPhone Operating System </AC>
+    <QC>Q150- What is the full form of BIOS?</QC>
+    <AC>Ans- Basic Input Output System </AC>
     
+
     </>
   );
 }
